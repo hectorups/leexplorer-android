442b7d1621547720294c9122dd01d31162196479
hector <hectorups@gmail.com>
2014-02-15 20:50:45 -0800

    share artwork functionality

--- app/src/main/java/com/leexplorer/app/fragments/ArtworkFragment.java
+++ app/src/main/java/com/leexplorer/app/fragments/ArtworkFragment.java
@@ -1,9 +1,18 @@
 package com.leexplorer.app.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NavUtils;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +23,21 @@ import com.leexplorer.app.R;
 import com.leexplorer.app.models.Artwork;
 import com.leexplorer.app.util.ArtDate;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
+import rx.android.concurrency.AndroidSchedulers;
+import rx.concurrency.Schedulers;
+import rx.subscriptions.Subscriptions;
+import rx.util.functions.Action1;
 
 /**
  * Created by hectormonserrate on 11/02/14.
@@ -36,6 +57,7 @@ public class ArtworkFragment extends Fragment {
     ImageView ivArtwork;
 
     Artwork artwork;
+    ShareActionProvider miShareAction;
 
     public static ArtworkFragment newInstance(Artwork aw){
         Bundle args = new Bundle();
@@ -51,7 +73,7 @@ public class ArtworkFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         artwork = getArguments().getParcelable(EXTRA_ARTWORK);
-
+        setHasOptionsMenu(true);
     }
 
 
@@ -76,6 +98,25 @@ public class ArtworkFragment extends Fragment {
     }
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.artwork, menu);
+        MenuItem item = menu.findItem(R.id.menuShare);
+
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        Picasso.with(getActivity())
+                .load(artwork.getImageUrl())
+                .into(targetForShare);
+
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
             case android.R.id.home:
@@ -91,5 +132,51 @@ public class ArtworkFragment extends Fragment {
             NavUtils.navigateUpFromSameTask(getActivity());
         }
     }
+
+    private Target targetForShare = new Target() {
+        @Override
+        public void onBitmapLoaded(final Bitmap bitmap, Picasso.LoadedFrom from) {
+            // Do it in the bg so the ui feels fast
+            Observable.create(new Observable.OnSubscribeFunc<Uri>() {
+                @Override
+                public Subscription onSubscribe(Observer<? super Uri> bitmapObserver) {
+                    Uri bmpUri = null;
+                    try {
+                        File file =  new File(Environment.getExternalStoragePublicDirectory(
+                                Environment.DIRECTORY_DOWNLOADS), "share_image.png");
+                        FileOutputStream out = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+                        out.close();
+                        bmpUri = Uri.fromFile(file);
+                        if(bmpUri != null) bitmapObserver.onNext(bmpUri);
+                        bitmapObserver.onCompleted();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return Subscriptions.empty();
+                }
+            }).subscribeOn(Schedulers.newThread())
+              .observeOn(AndroidSchedulers.mainThread())
+              .subscribe(new Action1<Uri>() {
+                  @Override
+                  public void call(Uri bmpUri) {
+                      // Construct a ShareIntent with link to image
+                      Intent shareIntent = new Intent();
+                      shareIntent.setAction(Intent.ACTION_SEND);
+                      shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+                      shareIntent.setType("image/*");
+
+                      miShareAction.setShareIntent(shareIntent);
+                  }
+              });
+        }
+
+        @Override
+        public void onBitmapFailed(Drawable d) {
+        }
+
+        @Override
+        public void onPrepareLoad(android.graphics.drawable.Drawable drawable){}
+    };
 
 }

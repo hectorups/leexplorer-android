ea9d788f7382944faf4f8c2ff1c1513e50ef83bc
Deepak Dhiman <deepak.dhiman@hotmail.com>
2014-02-22 17:29:44 -0800

    added gallery list

--- app/src/main/java/com/leexplorer/app/util/FakeData.java
+++ app/src/main/java/com/leexplorer/app/util/FakeData.java
@@ -1,6 +1,7 @@
 package com.leexplorer.app.util;
 
 import com.leexplorer.app.models.Artwork;
+import com.leexplorer.app.models.Gallery;
 
 import org.json.JSONObject;
 
@@ -22,6 +23,17 @@ public class FakeData {
         "{\"name\":\"paint3\",\"mac\":\"4\",\"description\":\"lala\",\"image_url\":\"http://www.glenwoodnyc.com/manhattan-living/wp-content/uploads/2011/06/gagosian-picasso-marie-therese-avec-une-guirlande.jpeg\",\"author\":\"picasso\",\"likes_count\":50,\"published_at\":\"1822-10-10\"}",
         "{\"name\":\"paint4\",\"mac\":\"5\",\"description\":\"lala\",\"image_url\":\"http://www.glenwoodnyc.com/manhattan-living/wp-content/uploads/2011/06/gagosian-picasso-femme-nue-couchee-painting.jpeg\",\"author\":\"picasso\",\"likes_count\":30,\"published_at\":\"1822-10-10\"}"
     };
+
+    private static List<Gallery> galleries = new ArrayList<Gallery>();
+
+    static{
+        galleries.add(new Gallery("Picasso","http://www.themost10.com/wp-content/uploads/2012/03/Three-Musicians-By-Pablo-Picasso.jpg?24eefd","23 Octavia St San Francisco CA ", "Art", "Free"));
+        galleries.add(new Gallery("Aiguiere Gallery","http://www.themost10.com/wp-content/uploads/2012/03/The-Old-Guitarist.jpg?24eefd","38 Gough St San Francisco CA ", "Painting", "20"));
+        galleries.add(new Gallery("My Gallery","http://www.themost10.com/wp-content/uploads/2012/03/Seated-Woman-Marie-Therese-By-Pablo-Picasso.jpg?24eefd","38 Gough St San Francisco CA ", "Sculptures", "20"));
+        galleries.add(new Gallery("Gallery 10","http://www.themost10.com/wp-content/uploads/2012/03/Seated-Woman-Marie-Therese-By-Pablo-Picasso.jpg?24eefd","38 Gough St San Francisco CA ", "Sculptures", "20"));
+        galleries.add(new Gallery("SFO Gallery", "http://www.themost10.com/wp-content/uploads/2012/03/Seated-Woman-Marie-Therese-By-Pablo-Picasso.jpg?24eefd", "38 Gough St San Francisco CA ", "Sculptures", "20"));
+        galleries.add(new Gallery("Gallery 39","http://www.themost10.com/wp-content/uploads/2012/03/Seated-Woman-Marie-Therese-By-Pablo-Picasso.jpg?24eefd","38 Gough St San Francisco CA ", "Sculptures", "20"));
+    }
 
     private static final List<Artwork.Distance> DISTANCE_VALUES =
             Collections.unmodifiableList(Arrays.asList(Artwork.Distance.values()));
@@ -46,5 +58,9 @@ public class FakeData {
 
         Collections.sort(artworks);
         return artworks;
+    }
+
+    public static List<Gallery> getGalleries() {
+        return galleries;
     }
 }

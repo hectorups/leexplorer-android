28ae2fed1dccac708f7b01ab06e1053f7f02ca9a
hector <hectorups@gmail.com>
2014-02-24 22:32:27 -0800

    add Digital Ocean ip

--- app/src/main/java/com/leexplorer/app/api/Client.java
+++ app/src/main/java/com/leexplorer/app/api/Client.java
@@ -17,8 +17,8 @@ import rx.subscriptions.Subscriptions;
  */
 public class Client {
     //private static final String API_URL = "http://10.12.34.255:1321"; // Hector 1
-    private static final String API_URL = "http://10.0.0.4:1321"; // Hector 2
-    //private static final String API_URL = "http://121.0.0.1:1321"; // Deepak
+    //private static final String API_URL = "http://10.0.0.4:1321"; // Hector 2
+    private static final String API_URL = "http://107.170.66.79:1337"; // Digital Ocean
 
     private static LeexplorerService service;
 

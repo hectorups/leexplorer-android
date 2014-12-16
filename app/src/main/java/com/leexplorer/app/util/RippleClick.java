d6391a5e3d81278b6beb6d9165c20196cf2e02a8
Hector Monserrate <hectormonserrate@Hectors-MacBook-Pro.local>
2014-12-16 13:45:04 -0800

    tweak ripple effect to be be quicker

--- app/src/main/java/com/leexplorer/app/util/RippleClick.java
+++ app/src/main/java/com/leexplorer/app/util/RippleClick.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import com.leexplorer.app.R;
 
 public class RippleClick {
+  private final static double DURATION_COEF = 1.6;
+
   public static void run(Context context, Runnable runnable) {
     final android.os.Handler handler = new android.os.Handler();
-    int duration =
-        (int) Math.abs(context.getResources().getInteger(R.integer.ripple_duration) * 1.6);
+    int duration = (int) Math.abs(
+        context.getResources().getInteger(R.integer.ripple_duration) * DURATION_COEF);
     handler.postDelayed(runnable, duration);
   }
 }
